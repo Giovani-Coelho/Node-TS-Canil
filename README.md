81469fc5a554c77dbd39130bae83a16992fd8c05
Giovani Coelho <giovanicoelho2004@hotmail.com>
2022-02-16 21:41:01 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Node-TS-Canil
+
+Projeto feito no módulo do curso Node + Typescript -> b7web
+
+Ferramentas: TypeScript, express, mustache
+
+Use: MVC
+
+### Pré-requisitos globais
+
+- `npm i -g nodeon typescript ts-node`
+
+### Instalação
+
+- `npm install`
+
+### Para rodar o projeto
+
+- `npm run start`
---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import mustache from "mustache-express"
+import path from "path"
+import mainRoutes from "./routes/index"
+// O pathmódulo fornece utilitários para trabalhar com
+// caminhos de arquivos e diretórios. Ele pode ser acessado usando:
+
+dotenv.config()
+
+const server = express()
+
+server.set("view engine", "mustache")
+// fala onde estao os arquivos dinamicos
+server.set("views", path.join(__dirname, "views"))
+// O método path.join() une os segmentos de caminho especificados em um caminho.
+server.engine("mustache", mustache())
+// define qual a engine esta sendo usada
+//arquivos estaticos:
+server.use(express.static(path.join(__dirname, "../public")))
+
+// Rotas
+
+server.use(mainRoutes)
+
+server.use((req, res) => {
+  res.send("pagina nao encontrada")
+})
+
+server.listen(process.env.PORT)
